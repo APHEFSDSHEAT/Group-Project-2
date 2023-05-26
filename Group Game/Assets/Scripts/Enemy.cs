11d1441cc73fa5380ff318514cd5fa0e413b6df2
Kirboisverycool <1522838@edu.burnabyschools.ca>
2023-05-26 14:59:09 -0700

    Enemy Movement

---
+++ Group Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            moveSpeed = -moveSpeed;
+            FlipEnemyFacing();
+        }
+    }
+
+    private void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+}

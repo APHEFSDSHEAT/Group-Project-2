51fbc934bc86723f0365025cc7c617043be444a3
Kirboisverycool <1522838@edu.burnabyschools.ca>
2023-06-19 14:18:59 -0700

    marionette scene

---
+++ Group Game/Assets/Scripts/NewEnemy.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemy : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidbody;
+    [SerializeField] float deathTime = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        Die();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            moveSpeed = -moveSpeed;
+            FlipEnemyFacing();
+        }
+        DamageDealer otherDamageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!otherDamageDealer) { return; }
+        ProcessHit(otherDamageDealer);
+    }
+
+    private void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+
+    private void ProcessHit(DamageDealer otherDamageDealer)
+    {
+        health -= otherDamageDealer.GetDamage();
+        otherDamageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<EnemySpawning>().SpawnAnotherEnemy(deathTime);
+        Destroy(gameObject, deathTime);
+    }
+}
